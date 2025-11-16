48174c1e497a0adcf907497d3fa81ce940974f8c
naresh7991 <49777651+naresh7991@users.noreply.github.com>
2025-11-16 15:50:19 +0530

    add cocktail list section

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { cocktailLists, mockTailLists } from '../../constants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const Cocktails = () => {
+    useGSAP(()=>{
+        const parallexTimeline = gsap.timeline({
+            scrollTrigger:{
+            trigger:'#cocktails',
+            start:'top 30%',
+            end: 'bottom 80%',
+            scrub: true,
+            }
+        })
+        parallexTimeline.from('#c-left-leaf',{
+            x:-100,y:100
+        })
+        .from('#c-right-leaf',{
+            x:100,y:100
+        })
+    
+ },[])
+  return (
+    <section id="cocktails" className='noisy'>
+<img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+	 <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+        <div className='list'>
+            <div className='popular'>
+                <h2>Most Popular cocktails:</h2>
+                <ul>
+                    {cocktailLists.map((drink)=>(
+                        <l1 key={drink.name}>
+                            <div className='md:me-28'>
+                                <h3>{drink.name}</h3>
+                                <p>{drink.country} | {drink.detail}</p>
+                            </div>
+                            <span>- {drink.price}</span>
+                        </l1>
+                    ))}
+                </ul>
+            </div>
+            <div className='loved'>
+                <h2>Most loved mockTailLists:</h2>
+                <ul>
+                    {mockTailLists.map((drink)=>(
+                        <l1 key={drink.name}>
+                            <div className='md:me-28'>
+                                <h3>{drink.name}</h3>
+                                <p>{drink.country} | {drink.detail}</p>
+                            </div>
+                            <span>- {drink.price}</span>
+                        </l1>
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Cocktails
