80916ea68e6db8cfd4959fe8c82e31ca4fcb875e
naresh7991 <49777651+naresh7991@users.noreply.github.com>
2025-11-21 01:12:50 +0530

    added masked section

---
+++ src/components/Art.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { featureLists, goodLists } from '../../constants'
+import { useMediaQuery } from 'react-responsive'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const Art = () => {
+    const isMobile = useMediaQuery({maxWidth:767})
+    useGSAP(()=>{
+        const start = isMobile?'top 20%':'top top';
+        const maskTimeLine = gsap.timeline({
+            scrollTrigger:{
+                trigger:'#art',
+                start,
+                end:'bottom center',
+                scrub: 1.5,
+                pin: true
+            }
+        })
+        maskTimeLine.to('.will-fade',{
+            opacity:0, stagger:0.2,ease:'power1.inOut',
+        })
+        .to('.masked-img',{
+            scale:1.3, maskPosition:'center', maskSize:'400%',duration:1,ease:'power1.inOut'
+        })
+        .to('#masked-content',{
+            opacity:1,
+            duration:1,ease:'power1.inOut'
+
+        })
+    })
+  return (
+    <div id="art">
+        <div className='container mx-auto h-full pt-20'>
+        <h2 className='will-fade'>
+            The Art
+        </h2>
+        <div className='content'>
+            <ul className='space-y-4 will-fade'>
+                {goodLists.map((features,index)=>(
+                    <li key={index} className='flex item-center gap-2'>
+                        <img src = "/images/check.png" alt="check"/>
+                        <p>{features}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className='cocktail-img'>
+                <img
+                src = '/images/under-img.jpg'
+                alt="cocktail"
+                className='abs-center masked-img size-full object-contain'
+                />
+            </div>
+            <ul className='space-y-4 will-fade'>
+                {featureLists.map((feature,index)=>(
+                    <li key={index} className='flex item-center gap-2'>
+                        <img src = "/images/check.png" alt="check"/>
+                        <p className='md:w-fit w-60'>{feature}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div className='masked-container'>
+            <h2 className='will-fade '>Slip worthy perfections</h2>
+            <div id="masked-content">
+                <h3>Made with craft poured with Passion</h3>
+                <p> this isnt drink it beautiful experiences </p>
+            </div>
+
+        </div>
+        </div>      
+    </div>
+  )
+}
+
+export default Art
